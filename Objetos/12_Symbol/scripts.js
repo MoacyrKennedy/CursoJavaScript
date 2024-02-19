40e7345e39824a5da3fd5a7b9e4d48f7195e61a3
MoacyrGrm <kennedymoacyr@gmail.com>
2024-02-19 10:09:24 -0300

    Update scripts.js

--- Objetos/12_Symbol/scripts.js
+++ Objetos/12_Symbol/scripts.js
@@ -17,9 +17,11 @@ Cachorro.prototype[patas]= 4;
 
 let labrador = new Cachorro('Labrador',  'Amarelo');
 
-console.log(labrador.patas);
 
 labrador.latir();
 
 console.log(Cachorro.prototype.raca);
 console.log(labrador.raca);
+
+//Acessando symbol
+console.log(Cachorro.prototype[patas]);
