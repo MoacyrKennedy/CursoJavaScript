03b5599d04a3fdacda5cda65f98301a1327bb476
MoacyrGrm <kennedymoacyr@gmail.com>
2024-02-21 08:58:31 -0300

    Update scripts.js

--- ExpressoesRegulares/01_regex_test/scripts.js
+++ ExpressoesRegulares/01_regex_test/scripts.js
@@ -5,3 +5,15 @@ const reg1 = new RegExp('bola');
 
 console.log(reg1.test("Tem bola?"));
 console.log(reg1.test("Não tem"));
+
+const reg2 = /bola/;
+
+let text = 'Tem bola na cesta';
+
+console.log(reg2.test("Tem bola?"));
+console.log(reg2.test("Não tem"));
+
+console.log(reg2.test(text));
+
+console.log(/quadrado/.test("Onde tem um quadrado"));
+console.log(/quadrado/.test("34234445645645645456456456456456"));
