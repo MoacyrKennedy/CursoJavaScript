8bd7b014c3e00b54144efe2c396ef72e56bb47d0
Moacyr kennedy <105392455+MoacyrKennedy@users.noreply.github.com>
2024-02-26 22:37:24 -0300

    d

---
+++ ExpressoesRegulares/06_operador_plus/scripts.js
@@ -0,0 +1,9 @@
+const umOuMaisNumeros = /\d+/;
+
+console.log(umOuMaisNumeros.test("1518"));
+console.log(umOuMaisNumeros.test(""));
+console.log(umOuMaisNumeros.test("asdadasd"));
+console.log(umOuMaisNumeros.test("1"));
+console.log(umOuMaisNumeros.test("151132325421322212312238"));
+
+
