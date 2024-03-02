148fbcc10540ec5fd567c2cb819298dc3e521d8b
Moacyr kennedy <105392455+MoacyrKennedy@users.noreply.github.com>
2024-03-02 10:01:36 -0300

    validationMaiuscula

---
+++ ExpressoesRegulares/Exercicios/01_exercicio/scripts.js
@@ -0,0 +1,8 @@
+const validarMaiuscula = /[A-Z]/;
+
+console.log(validarMaiuscula.test("testando"));
+console.log(validarMaiuscula.test("TESTANDO"));
+console.log(validarMaiuscula.test("123"));
+
+
+
