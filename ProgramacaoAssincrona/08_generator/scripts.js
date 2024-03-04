2910a9b57c75fe269ed403ec35d6f3041b75c0f7
Moacyr kennedy <105392455+MoacyrKennedy@users.noreply.github.com>
2024-03-04 20:01:12 -0300

    Generator

---
+++ ProgramacaoAssincrona/08_generator/scripts.js
@@ -0,0 +1,11 @@
+function* criadorId() {
+    let id = 0;
+    while(true) {
+        yield id++;
+    }
+}
+
+let criarId = criadorId();
+
+console.log(criarId.next().value);
+console.log(criarId.next().value);
